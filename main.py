71a0a1f38ea652b97dec943d74c684694e4bfa68
blob#0005 <88004068+blob0005@users.noreply.github.com>
2022-05-03 15:56:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ except Exception:
     print("Missing Module(s)")
     input("")
     exit()
+colorama.init(autoreset=True)
 def item():
-    colorama.init(autoreset=True)
     while True:
         try:
-            id = input("Enter Id: ")
+            id = input("Enter  Item Id: ")
             id = int(id)
             id = str(id)
             break
@@ -47,17 +47,44 @@ def item():
     try:
         print(colorama.Fore.GREEN + "Last Update To Item: " + str(json["Updated"]))
     except Exception:
-        print(colorama.Fore.RED + "Last Update To Item: Error Getting Last Update To Item")    
+        print(colorama.Fore.RED + "Last Update To Item: Error Getting Last Update To Item")
     try:
         print(colorama.Fore.GREEN + "Original Price: " + str(json["PriceInRobux"]))
     except Exception:
-        print(colorama.Fore.RED + "Price: Error Getting Price")        
+        print(colorama.Fore.RED + "Price: Error Getting Price")
     try:
         print(colorama.Fore.GREEN + "Is For Sale: " + str(json["IsForSale"]))
     except Exception:
         print(colorama.Fore.RED + "Is For Sale: Error Getting Is For Sale")
     print("All Info Printed")
     input("")
-    exit()
-print("Roblox Item Info Scraper (Roblox Api)")
-item()
+    return
+def user():
+    username = input("Input Username: ")
+    json1 = requests.get("https://api.roblox.com/users/get-by-username?username="+username).json()
+    try:
+        print(colorama.Fore.GREEN + "User Id: " + str(json1["Id"]))
+    except Exception:
+        print(colorama.Fore.RED + "User Id: ERROR Getting User Id")
+    try:
+        print(colorama.Fore.GREEN + "Avatar Uri: " + str(json1["AvatarUri"]))
+    except Exception:
+        print(colorama.Fore.RED + "Avatar Uri: ERROR Getting Avatar Uri")
+    try:
+        print(colorama.Fore.GREEN + "Avatar Final: " + str(json1["AvatarFinal"]))
+    except Exception:
+        print(colorama.Fore.RED + "Avatar Final: ERROR Getting Avatar Final")
+    try:
+        print(colorama.Fore.GREEN + "Is Online: " + str(json1["IsOnline"]))
+    except Exception:
+        print(colorama.Fore.RED + "Is Online: ERROR Getting Is Online")
+while True:
+    print("""
+    1. Item Scraper
+    2. User Scraper
+    """)
+    main = input("> ")
+    if main == "1":
+        item()
+    if main == "2":
+        user()
