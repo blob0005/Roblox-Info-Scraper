28446d5c50b49f3ade0c303f4262bcc4c42e434b
blob#0005 <88004068+blob0005@users.noreply.github.com>
2022-06-16 04:13:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 try:
     import os
     from os import system
-    system("title " + "Roblox Info Scraper")
+    system("title " + "Roblox Info Scraper,   Made By blob#0005,   Github: github.com/blob0005")
 except:
     pass
 try:
